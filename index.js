ed778b83f6a1d29e194afde9a591d93a7de899be
Tuhin Roy <troy0870@gmail.com>
2020-09-29 08:46:47 +0530

    add LEVEL1

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const { EMPTY } = require('rxjs');
 
+
+// LEVEL: 1: subscribe
+
+// Welcome to RxJS-fruits, where you write JavaScript / TypeScript code to mix your fruit juice!
+
+// The basis of Reactive Extensions is the Observable. It is a counterpart of an array. Instead of that Retaining data it streams the data to interested subscribers. (
+
+// An Observable is therefore only active if it is subscribed to with the subscribe-function. 
+
 const conveyorBelt = EMPTY;
 
 conveyorBelt.subscribe();
